2e8ec3579bb014d6551b80efaa4791317b30c198
anmolkabra <ak2426@cornell.edu>
2017-07-12 18:09:48 -0400

    added another window for writing posts

--- start-session.sh
+++ start-session.sh
@@ -12,26 +12,33 @@ function website {
 
     tmux start-server
     tmux new-session -d -s website -n files
-    tmux new-window -t website:1 -n vhost
-    tmux new-window -t website:2 -n jekyll
-    tmux new-window -t website:3 -n git
+    tmux new-window -t website:1 -n write
+    tmux new-window -t website:2 -n vhost
+    tmux new-window -t website:3 -n jekyll
+    tmux new-window -t website:4 -n git
 
     # files window
-    tmux send-keys -t website:0 "l" 'C-m'
+    tmux send-keys -t website:0 "l" C-m
 
-    # vhost window
-    tmux send-keys -t website:1 "cd /etc/apache2" C-m
-    tmux split-window -h -p 30 -t website:vhost
-    tmux send-keys -t website:1 "sudo service apache2 restart" C-m
+    # write window
+    tmux send-keys -t website:1 "l" C-m
+    tmux split-window -h -p 50 -t website:1
+    tmux send-keys -t website:1 "cd ./_posts && l" C-m
     tmux select-pane -t website:1.0
 
+    # vhost window
+    tmux send-keys -t website:2 "cd /etc/apache2" C-m
+    tmux split-window -h -p 30 -t website:2
+    tmux send-keys -t website:2 "sudo service apache2 restart" C-m
+    tmux select-pane -t website:2.0
+
     # jekyll window
-    tmux send-keys -t website:2 "bundle update" C-m
-    tmux send-keys -t website:2 "bundle install" C-m
-    tmux send-keys -t website:2 "bundle exec jekyll serve --watch" C-m
+    tmux send-keys -t website:3 "bundle update" C-m
+    tmux send-keys -t website:3 "bundle install" C-m
+    tmux send-keys -t website:3 "bundle exec jekyll serve --watch" C-m
 
     # git window
-    tmux send-keys -t website:3 "git status" C-m
+    tmux send-keys -t website:4 "git status" C-m
 
     tmux select-window -t website:0
     tmux -2 attach-session -t website
