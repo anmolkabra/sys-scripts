b4b6c277123f989a2eda034ed38bc9acf527ed65
anmolkabra <ak2426@cornell.edu>
2017-07-12 23:58:44 -0400

    does not create a new session or give error if the session exists

--- start-session.sh
+++ start-session.sh
@@ -5,43 +5,46 @@
 
 function website {
     # function to start a tmux session for my website dev environment
+    if (tmux has-session -t website 2> /dev/null); then
+        tmux attach -t website
+    else
+        # https://github.com/anmolkabra/gh_website
+        BASE="/home/anmolkabra/Desktop/gh_website"
+        cd $BASE
 
-    # https://github.com/anmolkabra/gh_website
-    BASE="/home/anmolkabra/Desktop/gh_website"
-    cd $BASE
+        tmux start-server
+        tmux new-session -d -s website -n files
+        tmux new-window -t website:1 -n write
+        tmux new-window -t website:2 -n vhost
+        tmux new-window -t website:3 -n jekyll
+        tmux new-window -t website:4 -n git
 
-    tmux start-server
-    tmux new-session -d -s website -n files
-    tmux new-window -t website:1 -n write
-    tmux new-window -t website:2 -n vhost
-    tmux new-window -t website:3 -n jekyll
-    tmux new-window -t website:4 -n git
+        # files window
+        tmux send-keys -t website:0 "l" C-m
 
-    # files window
-    tmux send-keys -t website:0 "l" C-m
+        # write window
+        tmux send-keys -t website:1 "l" C-m
+        tmux split-window -h -p 50 -t website:1
+        tmux send-keys -t website:1 "cd ./_posts && l" C-m
+        tmux select-pane -t website:1.0
 
-    # write window
-    tmux send-keys -t website:1 "l" C-m
-    tmux split-window -h -p 50 -t website:1
-    tmux send-keys -t website:1 "cd ./_posts && l" C-m
-    tmux select-pane -t website:1.0
+        # vhost window
+        tmux send-keys -t website:2 "cd /etc/apache2" C-m
+        tmux split-window -h -p 30 -t website:2
+        tmux send-keys -t website:2 "sudo service apache2 restart" C-m
+        tmux select-pane -t website:2.0
 
-    # vhost window
-    tmux send-keys -t website:2 "cd /etc/apache2" C-m
-    tmux split-window -h -p 30 -t website:2
-    tmux send-keys -t website:2 "sudo service apache2 restart" C-m
-    tmux select-pane -t website:2.0
+        # jekyll window
+        tmux send-keys -t website:3 "bundle update" C-m
+        tmux send-keys -t website:3 "bundle install" C-m
+        tmux send-keys -t website:3 "bundle exec jekyll serve --watch" C-m
 
-    # jekyll window
-    tmux send-keys -t website:3 "bundle update" C-m
-    tmux send-keys -t website:3 "bundle install" C-m
-    tmux send-keys -t website:3 "bundle exec jekyll serve --watch" C-m
+        # git window
+        tmux send-keys -t website:4 "git status" C-m
 
-    # git window
-    tmux send-keys -t website:4 "git status" C-m
-
-    tmux select-window -t website:0
-    tmux -2 attach-session -t website
+        tmux select-window -t website:0
+        tmux -2 attach-session -t website
+    fi
 } # end function website
 
 "$@"
