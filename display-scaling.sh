e7fb61f5516d7fc926929b98c90c5a15e37e6cd7
anmolkabra <ak2426@cornell.edu>
2018-08-03 00:52:10 -0400

    removed hardcoded conda activator, only activate when needed

--- display-scaling.sh
+++ display-scaling.sh
@@ -2,13 +2,12 @@
 
 # adapted from https://mensfeld.pl/2018/05/lenovo-thinkpad-x1-carbon-6th-gen-2018-ubuntu-18-04-tweaks/
 
-# adjust display size if hd monitor attached with laptop wqhd display
 gsettings set org.gnome.nautilus.icon-view default-zoom-level large
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 
+# adjust display size if hd monitor attached with laptop wqhd display
 xrandr --query  | grep ' connected' | grep 1920 > /dev/null
 result=$?
-
 if [ $result -ne 0 ]; then
 	# WQHD monitor
 	gsettings set org.gnome.desktop.interface text-scaling-factor 1.375
